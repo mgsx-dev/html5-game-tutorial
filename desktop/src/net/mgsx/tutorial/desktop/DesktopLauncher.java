a6ee33996a22bbb4662d86025389ba2fcf65b869
Germain AUBERT <germain.aubert@gmail.com>
2017-08-30 18:40:46 +0200

    libgdx project setup

---
+++ desktop/src/net/mgsx/tutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.tutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.tutorial.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
